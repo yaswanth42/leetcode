38b89b5357473263500bc566e4ccc88daf5263c2
yaswanth reddy <156218771+yaswanth42@users.noreply.github.com>
2025-05-21 14:23:55 +0530

    Time: 7 ms (88.79%) | Memory: 48 MB (89.66%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -8,12 +8,11 @@ class Solution {
 
             if (getSum(nums,mid)<=threshold) {
                 result=mid;
-                right=mid - 1;
+                right=mid-1;
             } else {
                 left=mid+1;
             }
         }
-
         return result;
     }
     private int getSum(int[] nums,int divisor) {
