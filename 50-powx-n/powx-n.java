aaf4fa08fd7cb1533e4f474bfc986608e3598eae
yaswanth reddy <156218771+yaswanth42@users.noreply.github.com>
2025-05-26 21:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (67.37%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        return fastPow(x,n);
+    }
+    private double fastPow(double x, int n) {
+        if (n == 0) return 1.0;
+        double half = fastPow(x, n / 2);
+        return (n % 2 == 0) ? half * half : half * half * x;
+    }
+}
